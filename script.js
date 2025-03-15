31101507995e5b02f97606306faf275f6b981bfe
Saba-Daryabari <sabadbdb@gmail.com>
2025-03-15 21:00:39 +0330

    initializing the project

---
+++ script.js
@@ -0,0 +1,28 @@
+const num = document.querySelector(".number");
+const btnPerv = document.querySelector(".btn-perv");
+const btnNext = document.querySelector(".btn-next");
+const btnReset = document.querySelector(".btn-reset");
+
+let currentNum = 0;
+
+const update = function () {
+  document.getElementById("number").textContent = currentNum;
+};
+
+btnNext.addEventListener("click", function () {
+  currentNum++;
+  update();
+});
+
+btnPerv.addEventListener("click", function () {
+  if (num.textContent > 0) {
+    currentNum--;
+    update();
+  } else {
+    return;
+  }
+});
+btnReset.addEventListener("click", function () {
+  currentNum = 0;
+  update();
+});
